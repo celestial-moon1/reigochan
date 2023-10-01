e0736757b64e1af545dbfb560d3d171ada6e7f51
Nguyen Quoc Huy <146612626+Huyaterminator@users.noreply.github.com>
2023-10-01 17:41:11 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+<script>
+    const audio = document.getElementById('background-audio');
+
+    // You can control the audio using JavaScript functions
+    function playAudio() {
+        audio.play();
+    }
+
+    function pauseAudio() {
+        audio.pause();
+    }
+</script>
