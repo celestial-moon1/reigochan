0bebcb544bb2b456b3edd9293782ea84f1d23358
yuzuruu <imnotgood79@gmail.com>
2025-05-12 14:36:54 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,10 @@
+// Configuration
+const config = {
+    // Image upload configuration
+    imgbb: {
+        apiKey: "ee3bf05be719bd26e91112dc1a9f5e1e"
+    }
+};
+
+// Export the configuration
+export default config; 
